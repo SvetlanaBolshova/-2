9cc9eed480f738ff14acc5077d40f4185ce6ebcd
SvetlanaBolshova <80790487+SvetlanaBolshova@users.noreply.github.com>
2021-03-21 18:46:00 +0300

    Add files via upload

---
+++ HM2Bolhova/BolshovaHM2.java
@@ -0,0 +1,23 @@
+package HM2Bolhova;
+
+public class BolshovaHM2 {
+
+    /**
+     * 1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+     * С помощью цикла и условия заменить 0 на 1, 1 на 0;
+     */
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.print("BEFORE: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+            if (arr[i] == 0) arr[i] = 1;
+            else arr[i] = 0;
+        }
+        System.out.print("\nAFTER:  ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+}
---
+++ HM2Bolhova/DZ2.java
@@ -0,0 +1,27 @@
+package HM2Bolhova;
+
+public class DZ2 {
+    /**
+     * 4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое)
+     *   и с помощью цикла(-ов) заполнить его диагональные элементы единицами;
+     */
+    public static void main(String[] args) {
+        int length = 5; // length of array
+
+        int[][] arr = new int[length][length];
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                if ((i == j) || (i == length - 1 - j)) {
+                    arr[i][j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
